2410246f1aad01e2cdc28df667b4e5fad853446d
Yuan WU <jjjjjimyuan@gmail.com>
2024-07-22 00:37:06 +0800

    Build: router init

--- app.js
+++ app.js
@@ -6,6 +6,27 @@ app.get('/', (req, res) => {
   res.send('Hello World')
 })
 
+app.get('/restaurant-list', (req, res) => {
+  res.send('Listing restaurants')
+})
+
+app.get('/restaurant-list/:id', (req, res) => {
+ res.send('detail page') 
+})
+
+app.post('/restaurant-list/new', (req, res) => {
+  res.send('add new restaurant')
+})
+
+app.put('/restaurant-list/:id/edit', (req, res) => {
+  res.send('modify restaurant')
+})
+
+app.delete('/restaurant-list/:id', (req, res) => {
+  res.send('delete restaurant')
+})
+
 app.listen(port, () => {
   console.log(`This app is running on http://localhost/${port}`)
 })
+
