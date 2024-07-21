76b4e8ae21b2ef009ec5e039b3ef744161cc327b
Yuan WU <jjjjjimyuan@gmail.com>
2024-07-22 01:38:37 +0800

    initialize handlebars  layout

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require('express')
 const app = express()
 
+const { engine} = require('express-handlebars')
+
 const db = require('./models')
 const Restaurant = db.Restaurant
 
 const port = 3000
 
+app.engine('.hbs', engine({ extname: '.hbs' }))
+app.set('view engine', '.hbs')
+app.set('views', './views')
+
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.render('index')
 })
 
 app.get('/restaurant-list', (req, res) => {
--- models/restaurant.js
+++ models/restaurant.js
@@ -1,8 +1,7 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
+const { Model, DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
   class Restaurant extends Model {
     /**
      * Helper method for defining associations.
@@ -13,11 +12,64 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
+
   Restaurant.init({
-    name: DataTypes.STRING
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    name_en: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    category: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    location: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    google_map: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    rating: {
+      type: DataTypes.FLOAT,
+      allowNull: true
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: true
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW
+    }
   }, {
     sequelize,
     modelName: 'Restaurant',
   });
+
   return Restaurant;
 };
