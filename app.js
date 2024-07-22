f8b3c380d411b87c9051baf8d65fbaf449256422
Yuan WU <jjjjjimyuan@gmail.com>
2024-07-22 12:34:15 +0800

    add detail page

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-const { engine} = require('express-handlebars')
+const { engine } = require('express-handlebars')
 
 const db = require('./models')
 const Restaurant = db.Restaurant
@@ -12,7 +12,16 @@ app.engine('.hbs', engine({ extname: '.hbs' }))
 app.set('view engine', '.hbs')
 app.set('views', './views')
 
+// 設置靜態文件目錄
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
+  return Restaurant.findAll({
+    attributes: ['id', 'name', 'category', 'image', 'rating'],
+    raw: true
+  })
+    .then((restaurants) => res.render('index', { restaurants }))
+    .catch((err) => res.status(422.).json(err))
   res.render('index')
 })
 
@@ -23,7 +32,13 @@ app.get('/restaurant-list', (req, res) => {
 })
 
 app.get('/restaurant-list/:id', (req, res) => {
-  res.send('detail page')
+  const id = req.params.id
+  return Restaurant.findByPk(id, {
+    attributes: ['id', 'name', 'category', 'image', 'phone', 'location', 'description'],
+    raw: true
+  })
+    .then((restaurant) => res.render('detail', { restaurant }))
+    .catch((err) => res.status(422).json(err))
 })
 
 app.post('/restaurant-list/new', (req, res) => {
