f9c150a477fd43ddca9ea7fac59b164fc9ecf54f
Yuan WU <jjjjjimyuan@gmail.com>
2024-07-28 17:50:01 +0800

    add error handling

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.get('/restaurant-list', (req, res) => {
     attributes: ['id', 'name', 'category', 'image', 'rating'],
     raw: true
   })
-    .then((restaurants) => res.render('index', { restaurants, message: req.flash('success')}))
+    .then((restaurants) => res.render('index', { restaurants, message: req.flash('success'), error: req.flash('error') }))
     .catch((err) => res.status(422.).json(err))
 })
 
@@ -50,7 +50,7 @@ app.get('/restaurant-list/:id', (req, res) => {
     attributes: ['id', 'name', 'category', 'image', 'phone', 'location', 'description'],
     raw: true
   })
-    .then((restaurant) => res.render('detail', { restaurant, message: req.flash('success') }))
+    .then((restaurant) => res.render('detail', { restaurant, message: req.flash('success'), error: req.flash('error') }))
     .catch((err) => res.status(422).json(err))
 })
 
@@ -60,53 +60,86 @@ app.get('/restaurant-list/:id/edit', (req, res) => {
     attributes: ['id', 'name', 'category', 'image', 'phone', 'location', 'description', 'rating'],
     raw: true
   })
-    .then((restaurant) => res.render('edit', { restaurant }))
+    .then((restaurant) => res.render('edit', { restaurant, error: req.flash('error')}))
 })
 
 app.post('/restaurant-list', (req, res) => {
-  const body = req.body
-  return Restaurant.create({
-    name: body.name,
-    category: body.category,
-    location: body.location,
-    phone: body.phone,
-    description: body.description,
-    image: body.image
-  })
-    .then(() => {
-      req.flash('success', '新增成功!')
-      return res.redirect('/restaurant-list')
+  try {
+    const body = req.body
+    return Restaurant.create({
+      name: body.name,
+      category: body.category,
+      location: body.location,
+      phone: body.phone,
+      description: body.description,
+      image: body.image
     })
-    .catch((err) => console.log(err))
+      .then(() => {
+        req.flash('success', '新增成功!')
+        return res.redirect('/restaurant-list')
+      })
+      .catch((err) => {
+        console.log(err)
+        req.flash('error', '新增失敗!')
+        return res.redirect('/restaurant-list')
+      })
+  } catch (error) {
+    console.log(err)
+    req.flash('error', '新增失敗!')
+    return res.redirect('/estaurant-list')
+  }
 })
 
 
 app.put('/restaurant-list/:id', (req, res) => {
 
-  const body = req.body
-  const id = req.params.id
+  try {
+    const body = req.body
+    const id = req.params.id
 
-  return Restaurant.update({
-    name: body.name,
-    location: body.location,
-    phone: body.phone,
-    description: body.description,
-    image: body.image,
-    rating: body.rating
-  }, { where: { id } })
-    .then(() => {
-      req.flash('success', '編輯成功!')
-      return res.redirect(`/restaurant-list/${id}`)
+    return Restaurant.update({
+      name: body.name,
+      location: body.location,
+      phone: body.phone,
+      description: body.description,
+      image: body.image,
+      rating: body.rating
+    }, { where: { id } })
+      .then(() => {
+        req.flash('success', '編輯成功!')
+        return res.redirect(`/restaurant-list/${id}`)
+      })
+      .catch((err) => {
+        console.log(err)
+        req.flash('error', '編輯失敗!')
+        return res.redirect('back')
     })
+  } catch (error) {
+    console.log(err)
+    req.flash('error', '編輯失敗!')
+    return res.redirect('back')
+    
+}
 })
 
 app.delete('/restaurant-list/:id', (req, res) => {
-  const id = req.params.id
-  return Restaurant.destroy({ where: { id } })
-    .then(() => {
-      req.flash('success', '刪除成功!')
-      return res.redirect('/')
+  try {
+    const id = req.params.id
+    return Restaurant.destroy({ where: { id } })
+      .then(() => {
+        req.flash('success', '刪除成功!')
+        return res.redirect('/')
+      })
+      .catch(() => {
+        console.log(err)
+        req.flash('error', '刪除失敗!')
+        return res.redirect('back')
     })
+  } catch (error) {
+    console.log(err)
+    req.flash('error', '刪除失敗!')
+    return res.redirect('back')
+  }
 })
 
 app.listen(port, () => {
