47356f340aa271b9077f79dc72bc690d14bb3278
Yuan WU <jjjjjimyuan@gmail.com>
2024-08-07 06:20:16 +0800

    add user-restaurant relationshsip migrations

--- models/restaurant.js
+++ models/restaurant.js
@@ -9,7 +9,7 @@ module.exports = (sequelize) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Restaurant.belongsTo(models.User)
     }
   }
 
