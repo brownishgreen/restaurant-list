8111b54d63563979399f261f133e8b743e00d1b3
brownishgreen <jjjjjimyuan@gmail.com>
2024-08-26 06:22:53 +0800

    router init

--- app.js
+++ app.js
@@ -7,6 +7,26 @@ app.get('/', (req, res) => {
   res.send('hello world')
 })
 
+app.get('/restaurant-list', (req, res) => {
+  res.send('listing restaurants')
+})
+
+app.get('/restaurant-list/:id', (req, res) => {
+  res.send('individual restaurant detail page')
+})
+
+app.get('/restaurant-list/new', (req, res) => {
+  res.send('add restaurant')
+})
+
+app.get('/restaurant-list/:id/edit', (req, res) => {
+  res.send(`edit restaurant ${id}`)
+})
+
+app.delete('/restaurant-list/:id', (req, res) => {
+  res.send('delete restaurant')
+})
+
 app.listen(port, () => {
   console.log(`App is running on http://localhost/${port}`)
 })
