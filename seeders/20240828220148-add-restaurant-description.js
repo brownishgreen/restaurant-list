fc0ba98e39110db904972698186efbbd5c586616
Jing Yuan WU <jjjjjimyuan@gmail.com>
2024-10-01 20:21:04 +0800

    Fix seeder table name

--- seeders/20240828220148-add-restaurant-description.js
+++ seeders/20240828220148-add-restaurant-description.js
@@ -21,6 +21,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('restaurants', null, {});
+    await queryInterface.bulkDelete('Restaurants', null, {});
   }
 };
